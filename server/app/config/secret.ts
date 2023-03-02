fb8a6bb0c68ec35ba583797e08fd4f4557d3f577
WebMelody <dev.mattew.chen@gmail.com>
2023-03-01 19:01:14 -0800

    update

--- server/app/config/secret.ts
+++ server/app/config/secret.ts
@@ -1,5 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
+var mongoUrl: string = process.env.MONGO_URI as string;
+const dbName: string = process.env.DB_NAME as string;
+if (process.env.NODE_ENV === "development") {
+    mongoUrl = mongoUrl.replace(/<db_name>/i, dbName.concat('_dev'));
+} else if (process.env.NODE_ENV === "production") {
+    mongoUrl = mongoUrl.replace(/<db_name>/i, dbName);
+}
+
 export const MONGO_URI = process.env.MONGO_URI;
-export const NODE_ENV = process.env.NODE_ENV;
+export const PORT = process.env.PORT;
