426d99232efa683e53dbc8bd7613a21d87bef42a
cagesanta <chandlercagex@gmail.com>
2023-03-13 15:03:03 -0700

    update: added total row in excel

--- client/src/config/export-excel.js
+++ client/src/config/export-excel.js
@@ -4,7 +4,11 @@ import xlsx from 'node-xlsx'
 
 export const makeExcelAndDownload = (dashboardData) => {
     var index = 1;
-
+    var totalRevenue = 0, totalSpend = 0;
+    dashboardData.forEach(item => {
+        totalRevenue += item.revenue;
+        totalSpend += item.spend;
+    });
     const data = [
         ['No', 'Name', 'Revenue', 'Spend', 'Profit', 'ROAS'],
         ...dashboardData.map(item => ([
@@ -14,9 +18,11 @@ export const makeExcelAndDownload = (dashboardData) => {
             "$" + item.spend,
             "$" + Number(item.profit).toFixed(2),
             Number(item.roas * 100).toFixed(2) + '%'       
-        ]))
+        ])),
+        ['', 'Total', "$" + totalRevenue, "$" + totalSpend, "$" + (totalRevenue - totalSpend), (totalRevenue / totalSpend) + "%"]
     ];
     var buffer = xlsx.build([{name: 'dashboard', data: data}]);
     const filename = "dashboard-" + dayjs.tz(dayjs(), "EST").format('YYYY-MM-DD') + '-' + Date.now();
+
     download(buffer, filename, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 }
